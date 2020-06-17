210ce0c992993a0ded2970fb871a310597ef9da3
Thanh <Thanh.Duong2503@gmx.de>
2020-06-17 16:03:46 +0200

    AddCode

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import "./LoadingSpinner";
+import LoadingSpinner from "./LoadingSpinner";
+
+function loadStudents(query) {
+  return fetch("http://localhost:8080/students?query="+query)
+      .then(response => response.json())
+}
 
 function App() {
+
+  const [students, setStudents]= React.useState([]);
+  const [loading, setLoading]= React.useState("");
+  const [query, setQuery]= React.useState("");
+
+  React.useEffect(() => {
+    setLoading("LOADING");
+    loadStudents(query)
+        .then(data => {
+      setStudents(data);
+      setLoading("SUCCESS")
+    }).catch( () => setLoading("FAILED"));
+  }, [query]);
+
   return (
     <div>
+      <input onChange={(event) => setQuery(event.target.value)}/>
 
+      {students.map(student => <div key={student.id}>{student.name}</div>)}
+      {loading === "LOADING" && <LoadingSpinner></LoadingSpinner>}
+      {loading === "FAILED" && <div>FAILED</div>}
     </div>
   );
 }
